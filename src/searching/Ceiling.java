8bc4ca3665db290b998df12f047ff5563284e15a
Thanmay <mdasthanmay@gmail.com>
2021-08-19 17:54:24 +0530

    Add Ceiling

---
+++ src/searching/Ceiling.java
@@ -0,0 +1,30 @@
+package searching;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 7, 9};
+        int num = 8;
+        int ans = ceiling(arr, num);
+        System.out.println(ans);
+    }
+
+    private static int ceiling(int[] arr, int target) {
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return arr[start];
+    }
+}
